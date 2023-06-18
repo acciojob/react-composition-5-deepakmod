fc0c185db74dcdf7f72696c559d5d3567f7a40b4
Deepak Modanwal <deepakmodanwal404@gmail.com>
2023-06-18 10:16:36 +0530

    init

---
+++ src/components/Tabs.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react';
+
+function Tabs({tab,content}){
+
+    const [activeTab,setActiveTab] = useState("");
+
+    return (
+
+    <div>
+
+        <ul>
+        {
+            tab.map((element)=>(
+                <li onClick={()=>{setActiveTab(element.content)}} >
+                    {element.title} {element.content}
+                </li>
+            ))
+        }
+        </ul>
+        
+        <p>This is the content for Tab {activeTab || content}.</p>
+    </div>
+
+    )
+}
+
+export default Tabs;
